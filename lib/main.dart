d5a5076c2a4e73a5bd9d31b891472914757af0c7
ItsAarif <itssaarif@gmail.com>
2025-04-11 18:38:44 +0530

    FAQ's Part

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:college_app/view/profiles/complete_profile_page.dart';
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:college_app/view/FirstPage.dart';
 
 void main() {
   runApp(DevicePreview(builder: (context) => MyApp()));
@@ -13,7 +12,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(debugShowCheckedModeBanner: false, home: Firstpage());
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: CompleteProfilePage(),
+    );
   }
 }
 
