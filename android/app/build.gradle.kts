f2601d92d2dc7a9a282321aa548b92f544e10de0
namrah <namrahsarfaraz2006@gmail.com>
2025-04-15 21:56:39 +0530

    firebase gooogle signin

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 android {
     namespace = "com.TalentConnect.college_app"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = "27.0.12077973"
+    ndkVersion = "29.0.13113456"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
