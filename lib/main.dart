f516161c515e959b0795e3704d857eac15d3d602
skhasan536 <hasan.shaikh@wybrid.com>
2025-05-07 12:24:42 +0530

    Revert "Connection 10 percent"
    This reverts commit a91eaeff4aeeeb64ddc21f455b5eef588e4e075a.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:college_app/view/SignUpLogin/FirstPage.dart';
 import 'package:college_app/view/home_page.dart';
 import 'package:college_app/view_model/themeController.dart';
-import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -10,7 +9,7 @@ Future<void> main() async {
   Get.put(ThemeController());
   WidgetsFlutterBinding.ensureInitialized();
   runApp(
-      DevicePreview(builder: (context)=>MyApp())
+      MyApp()
   );
 }
 
--- lib/view_model/data_loader.dart
+++ lib/view_model/data_loader.dart
@@ -1,7 +1,5 @@
 import 'package:get/get.dart';
 
-import '../model/college.dart';
-
 class Loader extends GetxController{
   Rx<bool> isLoading = false.obs;
 }
